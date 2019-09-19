db1ed066c6f09c87019fcf183122557825f53b89
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-09-18 23:02:09 -0700

    add initialAp in the character object and make some changes for the value to ensure each character can win and lose.

--- assets/javascript/characters.js
+++ assets/javascript/characters.js
@@ -1,6 +1,7 @@
+/** make some changes on the initialAp and cap value to ensure every character has the chance to win and lose */
 var characters = [
-    {name: "Obi-Wan Kenobi", id: "obi-wan-grid", hp: 120, ap: 15, cap: 20, imageUrl: "assets/images/obi-wan.jpeg"},
-    {name: "Luke Skywalker", id: "luke-grid", hp: 100, ap: 6, cap: 15, imageUrl: "assets/images/luke.jpeg"},
-    {name: "Darth Sidious", id: "sidious-grid", hp: 150, ap: 15, cap: 20, imageUrl: "assets/images/sidious.jpeg"},
-    {name: "Darth Maul", id: "maul-grid", hp: 180, ap: 20, cap: 25, imageUrl: "assets/images/maul.jpeg"},
+    {name: "Obi-Wan Kenobi", id: "obi-wan-grid", hp: 120, initialAp: 8, ap: 0, cap: 20, imageUrl: "assets/images/obi-wan.jpeg"},
+    {name: "Luke Skywalker", id: "luke-grid", hp: 100, initialAp: 15, ap: 0, cap: 25, imageUrl: "assets/images/luke.jpeg"},
+    {name: "Darth Sidious", id: "sidious-grid", hp: 150, initialAp: 8, ap: 0, cap: 10, imageUrl: "assets/images/sidious.jpeg"},
+    {name: "Darth Maul", id: "maul-grid", hp: 180, initialAp: 5, ap: 0, cap: 20, imageUrl: "assets/images/maul.jpeg"},
 ];
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,6 +26,9 @@ $(document).ready(function () {
     /** event handler of figure clicking */
     /** !!! this line is important for dynamically added element event handler !!!*/
     $(".container").on("click", ".character-group", function () {
+        if (status === "won" || status === "lost"){
+            return;
+        }
         /** if you character is empty, select character*/
         if (playerObj === null) {
             for (var i = 0; i < characterToSelect.length; i++) {
@@ -56,15 +59,18 @@ $(document).ready(function () {
 
     $("#btn-attack").on("click", function () {
         if (status === "ready" || status === "inprogress") {
+            playerObj.ap += playerObj.initialAp;
             playerObj.hp -= defenderObj.cap;
             defenderObj.hp -= playerObj.ap;
-            playerObj.ap += playerObj.ap;
             status = "inprogress";
 
             refreshWindow(status);
+
+
             /** Check if defender is defeated*/
             if (defenderObj.hp <= 0) {   
                 status = "defeated";
+                playerObj.hp += defenderObj.cap;
                 refreshWindow(status);
                 defenderObj = null;
             }
@@ -73,11 +79,15 @@ $(document).ready(function () {
             if (enemies.length === 0 && defenderObj === null) {
                 status = "won";
                 refreshWindow(status);
+                playerObj = null;
+                defenderObj = null;
             }
             /** check if lost */
             else if (playerObj.hp <= 0) {
                 status = "lost";
                 refreshWindow(status);
+                playerObj = null;
+                defenderObj = null;
             }
         }
     });
@@ -91,137 +101,119 @@ $(document).ready(function () {
      *  defeated - current defender hp is less than 0
      *  won - enemy array is empty
     */
-    function refreshWindow(status) {
-        var infoText;
-        var isRestartButton;
-        if (status === "start") {
-            for (var i = 0; i < characterToSelect.length; i++) {
-                var figToSelect = createFigure(characterToSelect[i]);
-                $("#character-grid").append(figToSelect);
-            }
-            $("#player-grid").empty();
-            $("#enemy-grid").empty();
-            $("#defender-grid").empty();
-            infoText = null;
-            isRestartButton = false;
+   function refreshWindow(status) {
+    var infoText;
+    var isRestartButton;
+    if (status === "start") {
+        for (var i = 0; i < characterToSelect.length; i++) {
+            var figToSelect = createFigure(characterToSelect[i]);
+            $("#character-grid").append(figToSelect);
         }
-        else if (status === "ready") {
-            infoText = null;
-            isRestartButton = false;
-            var playerDiv = $("#" + playerObj.id);
-            $("#player-grid").append(playerDiv);
-            if (defenderObj !== null) {
-                var defenderDiv = $("#" + defenderObj.id);
-                $("#defender-grid").append(defenderDiv);
-                defenderDiv.addClass("defender-group");
-            }
-            enemies.forEach(element => {
-                var enemyDiv = $("#" + element.id);
-                $("#enemy-grid").append(enemyDiv);
-                enemyDiv.addClass("enemy-group");
-            });
+        $("#player-grid").empty();
+        $("#enemy-grid").empty();
+        $("#defender-grid").empty();
+        infoText = null;
+        isRestartButton = false;
+    }
+    else if (status === "ready") {
+        infoText = null;
+        isRestartButton = false;
+        var playerDiv = $("#" + playerObj.id);
+        $("#player-grid").append(playerDiv);
+        if (defenderObj !== null) {
+            var defenderDiv = $("#" + defenderObj.id);
+            $("#defender-grid").append(defenderDiv);
+            defenderDiv.addClass("defender-group");
+        }
+        enemies.forEach(element => {
+            var enemyDiv = $("#" + element.id);
+            $("#enemy-grid").append(enemyDiv);
+            enemyDiv.addClass("enemy-group");
+        });
 
 
-        }
-        else if (status === "inprogress") {
-            updateHP(playerObj);
-            updateHP(defenderObj);
-            infoText = "You attacked " + defenderObj.name + " for " + playerObj.ap + " damages. " +
-                defenderObj.name + " attacked you back for " + defenderObj.cap + " damage.";
-            isRestartButton = false;
-        }
-        else if (status === "defeated") {
-            updateHP(playerObj);
-            updateHP(defenderObj);
-            $("#defender-grid").empty();
-            infoText = "You have defeated " + defenderObj.name + ", you can choose to fight another enemy.";
-            isRestartButton = false;
-        }
-        else if (status === "lost") {
-            infoText = "You've been defeated...GAME OVER!!!";
-            isRestartButton = true;
-        }
-        else if (status === "won") {
-            infoText = "You Won!!! GAME OVER!!!";
-            isRestartButton = true;
-        }
-
-        /** update the explanation paragraph */
-        setInfoGrid(infoText);
-        setRestartButton(isRestartButton);
+    }
+    else if (status === "inprogress") {
+        updateHP(playerObj);
+        updateHP(defenderObj);
+        infoText = "You attacked " + defenderObj.name + " for " + playerObj.ap + " damages. " +
+            defenderObj.name + " attacked you back for " + defenderObj.cap + " damage.";
+        isRestartButton = false;
+    }
+    else if (status === "defeated") {
+        updateHP(playerObj);
+        updateHP(defenderObj);
+        $("#defender-grid").empty();
+        infoText = "You have defeated " + defenderObj.name + ", you can choose to fight another enemy.";
+        isRestartButton = false;
+    }
+    else if (status === "lost") {
+        infoText = "You've been defeated...GAME OVER!!!";
+        isRestartButton = true;
+    }
+    else if (status === "won") {
+        infoText = "You Won!!! GAME OVER!!!";
+        isRestartButton = true;
     }
 
-    /** create figure div */
-    function createFigure(obj){
-        var div = $("<div>");
-        div.addClass("character-group");
-        div.attr("id", obj.id);
+    /** update the explanation paragraph */
+    setInfoGrid(infoText);
+    setRestartButton(isRestartButton);
+}
 
-        var p1 = $("<p>").addClass("text-center figure-caption my-0");
-        p1.text(obj.name);
-        div.append(p1);
+/** create figure div */
+function createFigure(obj){
+    var div = $("<div>");
+    div.addClass("character-group");
+    div.attr("id", obj.id);
 
-        var img = $("<img>").addClass("w-100 h-75 m-0 figure-img img-fluid");
-        img.attr("src", obj.imageUrl);
-        div.append(img);
+    var p1 = $("<p>").addClass("text-center figure-caption my-0");
+    p1.text(obj.name);
+    div.append(p1);
 
-        var p2 = $("<p>").addClass("hp-value text-center figure-caption");
-        p2.text(obj.hp);
-        div.append(p2);
+    var img = $("<img>").addClass("w-100 h-75 m-0 figure-img img-fluid");
+    img.attr("src", obj.imageUrl);
+    div.append(img);
 
-        return div;
+    var p2 = $("<p>").addClass("hp-value text-center figure-caption");
+    p2.text(obj.hp);
+    div.append(p2);
+
+    return div;
+}
+
+/** set info-grid */
+function setInfoGrid(text) {
+    $("#info-grid").empty();
+    if (text !== null) {
+        var p1 = $("<p>");
+        p1.text(text);
+        $("#info-grid").append(p1);
     }
+}
 
-    /** set info-grid */
-    function setInfoGrid(text) {
-        $("#info-grid").empty();
-        if (text !== null) {
-            var p1 = $("<p>");
-            p1.text(text);
-            $("#info-grid").append(p1);
+/** set restart button */
+function setRestartButton(isAvailable) {
+    //$("#restart-grid").empty();
+    if (isAvailable) {
+        $("#btn-restart").css("visibility", "visible");
+    }
+    else{
+        $("#btn-restart").css("visibility", "hidden");
+    }
+}
+
+function updateHP(obj) {
+    var div = $("#" + obj.id);
+    for (var i = 0; i < div[0].childNodes.length; i++) {
+        if (typeof div[0].childNodes[i].classList !== "undefined" &&
+            div[0].childNodes[i].classList.contains("hp-value")) {
+            var hpElement = div[0].childNodes[i];
+            hpElement.textContent = obj.hp;
+            break;
         }
     }
-
-    /** set restart button */
-    function setRestartButton(isAvailable) {
-        //$("#restart-grid").empty();
-        if (isAvailable) {
-            // var btn_Restart = $("<button>");
-            // btn_Restart.text("Restart");
-            // btn_Restart.addClass("btn-restart");
-            // btn_Restart.attr("type","button");
-            // $("#restart-grid").append(btn_Restart);
-            $("#btn-restart").css("visibility", "visible");
-        }
-        else{
-            $("#btn-restart").css("visibility", "hidden");
-        }
-    }
-
-    function updateHP(obj) {
-        var div = $("#" + obj.id);
-        for (var i = 0; i < div[0].childNodes.length; i++) {
-            if (typeof div[0].childNodes[i].classList !== "undefined" &&
-                div[0].childNodes[i].classList.contains("hp-value")) {
-                var hpElement = div[0].childNodes[i];
-                hpElement.textContent = obj.hp;
-                break;
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 });
+
+
